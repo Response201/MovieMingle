c3ecac4228f7228d8d5326822088b3bdb5a0a928
jess <jessica.response201@gmail.com>
2024-06-05 13:03:29 +0200

    sql-server setup + create table > receipt + endpoint to create receipt

--- server/index.js
+++ server/index.js
@@ -2,16 +2,27 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const movies = require('./data.json');
-require('dotenv').config()
+const mysql = require('mysql2');
+require('dotenv').config();
 const port = 3000;
 app.use(cors());
 app.use(express.json());
+
+const pool = mysql.createPool({
+    host: process.env.host,
+    user: process.env.user,
+    password: process.env.pass,
+    database: process.env.db
+  });
+  
+
+
+
 
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
-
 
 app.get('/all', (req, res) => {
    
@@ -78,10 +89,56 @@ app.post('/api/genre/:genre', (req, res) => {
 
 
 
+/* spara köp i sql-server - Viktigt glöm inte att skicka med body: user, movies i post-anropet */
+
+    app.post('/makereceipt', async (req, res) => {
+ const { user, movies } = req.body; 
+
+    try {
+        const dt = new Date();
+        const dateNow = `${dt.getFullYear()}-${String(dt.getMonth() + 1).padStart(2, '0')}-${String(dt.getDate()).padStart(2, '0')} ${String(dt.getHours()).padStart(2, '0')}:${String(dt.getMinutes()).padStart(2, '0')}:${String(dt.getSeconds()).padStart(2, '0')}`;
+
+        for (const movie of movies) {
+            const { id } = movie;
+            await pool.execute(
+                'INSERT INTO receipt (username, productId, timeDate) VALUES (?, ?, ?)',
+                  [user, id, dateNow]
+            );
+        }
+        res.json({ message: 'Receipt created successfully' });
+    } catch (err) {
+        console.error('Error creating receipt:', err);
+        res.status(500).json({ error: 'Something went wrong' });
+    }
+    });
 
 
 
 
+    
+const createReceiptTable = () => {
+    const sql = `
+        CREATE TABLE IF NOT EXISTS receipt (
+            username VARCHAR(100) NOT NULL,
+            productId INT NOT NULL,
+            timeDate VARCHAR(50) NOT NULL,
+            PRIMARY KEY (username, productId, timeDate)
+        )
+    `;
+
+    pool.query(sql, (err, results) => {
+        if (err) {
+            console.error('Fel vid skapande av tabell:', err);
+        } else {
+    
+        }
+    });
+};
+
+
+createReceiptTable();
+      
+    
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
