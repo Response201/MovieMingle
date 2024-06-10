99e21ff6c74a4ef251b21265e31a161a529c07ea
Isabelle Schröder Jockovic <isabelle.sj@live.se>
2024-06-10 09:42:48 +0200

    fixed movies on startpage, added click function on movie

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,53 +1,64 @@
 import { useGlobalContext } from "../contexts/GlobalContext";
 import { useEffect, useState } from "react";
 import { FetchMovies } from "../functions/FetchMovies";
-export const Home =  () => {
-	/* Global Context => variablar går att nå i hela appen vid denna typ av import */
-	const { allMovies, totalPage, setAllMovies, setTotalPage } = useGlobalContext();
-	const [currentPage, setCurrentPage] = useState(1);
-	const [size, setSize] = useState(5);
+import { MoviePresentation } from "../components/MoviePresentation";
 
-	
-    const fetchMoviesData = async (page: number, size: number) => {
-        const { movies, totalPages } = await FetchMovies(page, size);
-        setAllMovies(movies);
-        setTotalPage(totalPages);
-    };
+export const Home = () => {
+  /* Global Context => variablar går att nå i hela appen vid denna typ av import */
+  const { allMovies, totalPage, setAllMovies, setTotalPage } =
+    useGlobalContext();
+  const [currentPage, setCurrentPage] = useState(1);
+  const [size, setSize] = useState(5);
 
-	/* make new fetch when page changes */
-	useEffect(() => {
-		fetchMoviesData(currentPage, size);
-	}, [currentPage, size]);
+  const fetchMoviesData = async (page: number, size: number) => {
+    const { movies, totalPages } = await FetchMovies(page, size);
+    setAllMovies(movies);
+    setTotalPage(totalPages);
+  };
 
+  /* make new fetch when page changes */
+  useEffect(() => {
+    fetchMoviesData(currentPage, size);
+  }, [currentPage, size]);
 
-	const handlePrevPage = () => {
-		if (currentPage > 1) {
-			setCurrentPage(currentPage - 1);
-		}
-	};
+  console.log(fetchMoviesData);
 
-	const handleNextPage = () => {
-		if (currentPage < totalPage) {
-			setCurrentPage(currentPage + 1);
-		}
-	};
+  const handlePrevPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
 
-	return (
-		<>
-			<h1>Home!</h1>
-			<button onClick={()=>setSize(3)}> 3 </button><button onClick={()=>setSize(5)}> 5 </button><button onClick={()=>setSize(8)}> 8 </button>
-			{allMovies.map((movie) => (
-				<div key={movie.id}>{movie.title}</div>
-			))}
+  const handleNextPage = () => {
+    if (currentPage < totalPage) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
 
-			<button onClick={handlePrevPage} disabled={currentPage === 1}>
-				Previous Page
-			</button>
-			<button onClick={handleNextPage} disabled={currentPage === totalPage}>
-				Next Page
-			</button>
+  let moviesHtml = allMovies.map((movie) => (
+    <MoviePresentation key={movie.id} movie={movie} />
+  ));
 
-			<p> {currentPage}/{totalPage}</p>
-		</>
-	);
+  return (
+    <>
+      <h1>Home!</h1>
+      <button onClick={() => setSize(3)}> 3 </button>
+      <button onClick={() => setSize(5)}> 5 </button>
+      <button onClick={() => setSize(8)}> 8 </button>
+
+      <div className="movies">{moviesHtml}</div>
+
+      <button onClick={handlePrevPage} disabled={currentPage === 1}>
+        Previous Page
+      </button>
+      <button onClick={handleNextPage} disabled={currentPage === totalPage}>
+        Next Page
+      </button>
+
+      <p>
+        {" "}
+        {currentPage}/{totalPage}
+      </p>
+    </>
+  );
 };
---
+++ frontend/src/pages/Movie.tsx
@@ -0,0 +1,7 @@
+export const Movie = () => {
+  return (
+    <>
+      <h1>One movie!</h1>
+    </>
+  );
+};
