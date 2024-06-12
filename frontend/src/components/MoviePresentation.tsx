d55d1ad3cc2424ce7d03022071053470f82288e1
Isabelle Schröder Jockovic <isabelle.sj@live.se>
2024-06-12 11:44:16 +0200

    added styling for selected movie

--- frontend/src/components/MoviePresentation.tsx
+++ frontend/src/components/MoviePresentation.tsx
@@ -14,19 +14,39 @@ export const MoviePresentation = (props: IMovieProps) => {
       onClick={() => {
         navigate("/movie/" + props.movie.id);
         if (location.pathname === "/") {
-          navigate("/movie/" + props.movie.id); //kolla om det funkar
+          navigate("/movie/" + props.movie.id);
         }
       }}
       className="movie"
     >
-      {/* <div
-        className="movie__image"
-        style={{ backgroundImage: `url(${props.movie.image})` }}
-      ></div> */}
-
-      <img className="movie__image" src={props.movie.image}></img>
-      <h3 className="movie__title">{props.movie.title}</h3>
-      <p className="movie__genre">{props.movie.genre.join(", ")}</p>
+      {location.pathname === "/movie/" + props.movie.id ? (
+        <>
+          <img
+            className="movie__image-selected"
+            src={props.movie.image}
+            alt={props.movie.title}
+          />
+          <div className="movie__info-wrapper">
+            <h3 className="movie__title-selected">{props.movie.title}</h3>
+            <p className="movie__genre-selected">
+              {props.movie.genre.join(", ")} | {props.movie.length} min
+            </p>
+            <p className="movie__description">{props.movie.description}</p>
+            <button className="movie__button-rent">
+              Rent: {props.movie.price} kr
+            </button>
+          </div>
+        </>
+      ) : (
+        <>
+          <img
+            className="movie__image"
+            src={props.movie.image}
+            alt={props.movie.title}
+          />
+          <h3 className="movie__title">{props.movie.title}</h3>
+        </>
+      )}
     </div>
   );
 };
