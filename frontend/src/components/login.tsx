a6f68da65de10aa6b2d16cda4cfe97259eceb079
Isabelle Schröder Jockovic <isabelle.sj@live.se>
2024-06-13 11:10:08 +0200

    changed google login button

--- frontend/src/components/login.tsx
+++ frontend/src/components/login.tsx
@@ -4,86 +4,80 @@ import Cookies from "js-cookie";
 import { useGlobalContext } from "../contexts/GlobalContext";
 
 interface GoogleResponse {
-    credential: string;
+  credential: string;
 }
 
-
-
 const loadScript = (src: string, onLoad: () => void) => {
-    const script = document.createElement("script");
-    script.src = src;
-    script.async = true;
-    script.onload = onLoad;
-    document.body.appendChild(script);
+  const script = document.createElement("script");
+  script.src = src;
+  script.async = true;
+  script.onload = onLoad;
+  document.body.appendChild(script);
 };
 
 export const Login = () => {
-    const { userSignedIn, setUserSignedIn } = useGlobalContext();
+  const { userSignedIn, setUserSignedIn } = useGlobalContext();
 
-
-
-	
-    function handleCallbackResponse(response: GoogleResponse) {
-		const userObject = jwtDecode(response.credential);
-        Cookies.set("jwtToken", response.credential, { expires: 1 / 24 });
-        const current = Date.now() / 1000;
-        if (userObject.exp && userObject.exp > current) {
-            setUserSignedIn(response.credential);
-        } else {
-            Cookies.set("jwtToken", "");
-        }
+  function handleCallbackResponse(response: GoogleResponse) {
+    const userObject = jwtDecode(response.credential);
+    Cookies.set("jwtToken", response.credential, { expires: 1 / 24 });
+    const current = Date.now() / 1000;
+    if (userObject.exp && userObject.exp > current) {
+      setUserSignedIn(response.credential);
+    } else {
+      Cookies.set("jwtToken", "");
     }
+  }
 
-    useEffect(() => {
-        loadScript("https://accounts.google.com/gsi/client", () => {
-            // Initialize Google Accounts ID
-            google.accounts.id.initialize({
-                client_id: import.meta.env.VITE_CLIENTID,
-                callback: handleCallbackResponse,
-            });
+  useEffect(() => {
+    loadScript("https://accounts.google.com/gsi/client", () => {
+      // Initialize Google Accounts ID
+      google.accounts.id.initialize({
+        client_id: import.meta.env.VITE_CLIENTID,
+        callback: handleCallbackResponse,
+      });
 
-            const signInDiv = document.getElementById("signInDiv");
-            if (signInDiv) {
-                google.accounts.id.renderButton(signInDiv, {
-                    type: "standard",
-                    theme: "outline",
-                    size: "large",
-                });
-            } else {
-                console.error('Element with id "signInDiv" not found.');
-            }
+      const signInDiv = document.getElementById("signInDiv");
+      if (signInDiv) {
+        google.accounts.id.renderButton(signInDiv, {
+          type: "standard",
+          theme: "outline",
+          size: "medium",
+          text: "signin",
+          shape: "pill",
         });
-    }, []);
+      } else {
+        console.error('Element with id "signInDiv" not found.');
+      }
+    });
+  }, []);
 
-    useEffect(() => {
-        const signInDiv = document.getElementById("signInDiv");
-        const signOutDiv = document.getElementById("signOutDiv");
+  useEffect(() => {
+    const signInDiv = document.getElementById("signInDiv");
+    const signOutDiv = document.getElementById("signOutDiv");
 
-       
-        if (signInDiv && signOutDiv) {
-            if (userSignedIn !== "") {
-                signInDiv.hidden = true;
-                signOutDiv.hidden = false;
-                
-            } else {
-                signInDiv.hidden = false;
-                signOutDiv.hidden = true;
-               
-            }
-        }
-    }, [userSignedIn]);
+    if (signInDiv && signOutDiv) {
+      if (userSignedIn !== "") {
+        signInDiv.hidden = true;
+        signOutDiv.hidden = false;
+      } else {
+        signInDiv.hidden = false;
+        signOutDiv.hidden = true;
+      }
+    }
+  }, [userSignedIn]);
 
-    const handleSignOut = () => {
-        Cookies.remove("jwtToken");
-        setUserSignedIn("");
-    };
+  const handleSignOut = () => {
+    Cookies.remove("jwtToken");
+    setUserSignedIn("");
+  };
 
-    return (
-        <section className="loginOut">
-            <div id="signInDiv"></div>
-            <button onClick={() => handleSignOut()} id="signOutDiv">
-                logga ut
-            </button>
-        </section>
-    );
+  return (
+    <section className="loginOut">
+      <div id="signInDiv"></div>
+      <button onClick={() => handleSignOut()} id="signOutDiv">
+        logga ut
+      </button>
+    </section>
+  );
 };
