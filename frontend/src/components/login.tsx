f81be1fb7deae12e66c285f7407e44a92099ad67
jess <jessica.response201@gmail.com>
2024-06-18 14:59:09 +0200

    remove code - not needed

--- frontend/src/components/login.tsx
+++ frontend/src/components/login.tsx
@@ -5,65 +5,11 @@ import { useGlobalContext } from "../contexts/GlobalContext";
 import { useNavigate } from "react-router-dom";
 
 
-/* interface GoogleResponse {
-  credential: string;
-}
-
-interface DecodedJWT {
-  email: string;
-  exp: number;
-}
-
-const loadScript = (src: string, onLoad: () => void) => {
-  const script = document.createElement("script");
-  script.src = src;
-  script.async = true;
-  script.onload = onLoad;
-  document.body.appendChild(script);
-}; */
 
 export const Login = () => {
   const { userSignedIn, setUserSignedIn } = useGlobalContext();
   const navigate = useNavigate()
-/* 
-  useEffect(() => {
-    function handleCallbackResponse(response: GoogleResponse) {
-      const userObject: DecodedJWT = jwtDecode(response.credential);
-      Cookies.set("jwtToken", response.credential, { expires: 1 / 24 });
-      const current = Date.now() / 1000;
 
-      if (userObject.exp && userObject.exp > current) {
-        setUserSignedIn(response.credential);
-        if (userObject.email) {
-          FetchRegUser(userObject.email, null, "google");
-          navigate("/")
-        }
-      } else {
-        Cookies.set("jwtToken", "");
-      }
-    }
-
-    loadScript("https://accounts.google.com/gsi/client", () => {
-      // Initialize Google Accounts ID
-      google.accounts.id.initialize({
-        client_id: import.meta.env.VITE_CLIENTID,
-        callback: handleCallbackResponse,
-      });
-
-      const signInGoogle = document.getElementById("signInGoogle");
-      if (signInGoogle) {
-        google.accounts.id.renderButton(signInGoogle, {
-          type: "standard",
-          theme: "outline",
-          size: "medium",
-          text: "signin",
-          shape: "pill",
-        });
-      } else {
-        console.error('Element with id "signInGoogle" not found.');
-      }
-    });
-  }, [setUserSignedIn]); */
 
   useEffect(() => {
     const signInDiv = document.getElementById("signInDiv");
